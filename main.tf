4f8ddf8b19aa20a7915f5254220c697a21053853
carlostapia <carlostapiaT3csup32@gmailcom>
2026-04-03 17:41:56 -0500

    prueba 4

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
     resource_group_name  = "rg-apppersonal-tfstate"
     storage_account_name = "stcarlosv3state"
     container_name       = "tfstate-apppersonal"
-    key                  = "lab21-pro.tfstate"
+    key                  = "tfstate.v3"
   }
 }
 
